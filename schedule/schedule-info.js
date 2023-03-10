74f69ce2b2191a9b575e46b530f2716c30dcc031
philipmjohnson <philipmjohnson@users.noreply.github.com>
2023-03-10 17:22:56 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,20 @@
+moreaEventData = [
+  {title: "E04: Introduction to Javascript with FreeCodeCamp", url: "/ics888s23/morea/example-javascript/experience-interactive-javascript-1.html", start: "2021-07-13T23:00"},
+  {title: "E05: isUnique", url: "/ics888s23/morea/example-javascript/experience-ctci.html", start: "2021-07-14T23:00"},
+  {title: "E06: Reflect on Javascript 1", url: "/ics888s23/morea/example-javascript/experience-javascript-1-technical-essay.html", start: "2021-07-15T23:00"},
+  {title: "A01: Assessment of simple Javascript skills", url: "/ics888s23/morea/example-javascript/assessment-javascript-1.html", start: "2021-07-16T09:00"},
+  {title: "E01: Environment configuration", url: "/ics888s23/morea/introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "E02: Introduce yourself", url: "/ics888s23/morea/introduction/experience-introduce-yourself.html", start: "2021-07-08T23:00"},
+  {title: "Introduction to ICS 888", url: "/ics888s23/modules/introduction", start: "2021-07-06", end: "2021-07-09T23:00"},
+  {title: "E03: Learn the command line", url: "/ics888s23/morea/introduction/experience-learn-the-command-line.html", start: "2021-07-09T23:00"},
+  {title: "E07: Reflect on smart questions", url: "/ics888s23/morea/example-open-source-software/experience-smart-questions.html", start: "2021-07-22T23:00"},
+  {title: "A02: Assess your ability to write effectively about open source software", url: "/ics888s23/morea/example-open-source-software/assessment-open-source-software.html", start: "2021-07-23T09:00"},
+  {title: "E08: What are the professional codes of software engineering ethics?", url: "/ics888s23/morea/example-ethics/experience-se-ethics-codes.html", start: "2021-07-26T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/ics888s23/morea/example-ethics/assessment-ethics.html", start: "2021-07-30T09:00"},
+  {title: "E10: Doing harm as a software engineer", url: "/ics888s23/morea/example-ethics/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "E09: Doing good as a software engineer", url: "/ics888s23/morea/example-ethics/experience-se-ethics-good.html", start: "2021-07-27T23:00"},
+  {title: "Cool features of Dart", url: "/ics888s23/morea/dart/experience-dart-cool-features.html", start: "2023-03-12T23:00"},
+  {title: "Introduction to Dart for Java Developers", url: "/ics888s23/morea/dart/experience-dart-for-java-developers.html", start: "2023-03-10T23:00"},
+  {title: "Solve a problem with Dart", url: "/ics888s23/morea/dart/assessment-dart.html", start: "2023-03-13T09:00"},
+  {title: "Introduction to Dart", url: "/ics888s23/modules/module-dart", start: "2023-03-07", end: "2023-03-14"}
+]
